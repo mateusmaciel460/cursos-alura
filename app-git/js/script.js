387e0d8eaf4da7cc5994057e9c423ecbbe3c518b
mateusmaciel460 <mateusmaciel460@gmail.com>
2025-01-12 01:04:48 -0300

    removendo arquivos antigos do curso de lógica

--- app-git/js/script.js
+++
@@ -1,7 +0,0 @@
-function exibirMensagemNaTela(tag, texto) {
-    let campo = document.querySelector(tag);
-    campo.innerHTML = texto;
-}
-
-exibirMensagemNaTela('h1', 'Git e Github');
-exibirMensagemNaTela('p', 'Aprendendo tudo sobre Git e Github')
