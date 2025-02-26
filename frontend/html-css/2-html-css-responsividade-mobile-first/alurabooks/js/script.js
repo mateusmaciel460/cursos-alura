02e36a22188afb501bbeffca4c7d2275e43204fe
mateusmaciel460 <mateusmaciel460@gmail.com>
2025-02-26 16:46:52 -0300

    atualizando 2-html-css-responsividade-mobile-first

--- frontend/html-css/2-html-css-responsividade-mobile-first/alurabooks/js/script.js
+++ frontend/html-css/2-html-css-responsividade-mobile-first/alurabooks/js/script.js
@@ -1,16 +1,28 @@
-function multiplicadorLogoRodape(id) {
-    let elemento = document.querySelector(`#elemento-multiplicador-${id}`);
+let elemento = document.querySelector('#elemento-multiplicador');
 
+function multiplicadorLogoRodape() {
     for (let contador = 0; contador < 4; contador++) {
         elemento.innerHTML += `
-            <li class="modelo__horizontal modelo__justifcado-alinhado--centro">
-                <img src="images/logo-caelum.png" alt="Logo - Caelum 01"/>
-                Caelum
-            </li>
+            <ul class="rodape__menu-lista modelo__vertical">
+                <li class="rodape__menu-titulo texto-maiusculo">Educação</li>
+                
+                <li class="modelo__horizontal modelo__justifcado-alinhado--centro">
+                    <img src="images/logo-caelum.png" alt="Logo da Caelum"/>
+                    <a href="#" class="modelo__link">Caelum</a>
+                </li>
+
+                <li class="modelo__horizontal modelo__justifcado-alinhado--centro">
+                    <img src="images/logo-caelum.png" alt="Logo da Caelum"/>
+                    <a href="#" class="modelo__link">Caelum</a>
+                </li>
+
+                 <li class="modelo__horizontal modelo__justifcado-alinhado--centro">
+                    <img src="images/logo-caelum.png" alt="Logo da Caelum"/>
+                    <a href="#" class="modelo__link">Caelum</a>
+                </li>
+            </ul>
         `;
     }
 }
 
-multiplicadorLogoRodape(1);
-multiplicadorLogoRodape(2);
-multiplicadorLogoRodape(3);
+multiplicadorLogoRodape();
